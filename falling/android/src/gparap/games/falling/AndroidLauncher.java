341e2de3786eebc7c0810aeeef0067d04f49fbd9
gparap <gparap@protonmail.com>
2022-11-04 15:25:03 +0200

    Update project with the latest tools
    Use the new major release of libGDX: version 1.11.0

--- falling/android/src/gparap/games/falling/AndroidLauncher.java
+++ falling/android/src/gparap/games/falling/AndroidLauncher.java
@@ -1,8 +1,10 @@
 package gparap.games.falling;
 
 import android.os.Bundle;
+
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import gparap.games.falling.Falling;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
