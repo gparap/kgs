446b6bd72ff00a6e04c843067bb56efc08c6b12d
gparap <gparap@protonmail.com>
2023-01-12 10:35:32 +0200

    Initial commit (rethink platformer)

---
+++ platformer/html/src/dmn/games/platformer/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package dmn.games.platformer.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import dmn.games.platformer.Platformer;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Platformer();
+        }
+}
