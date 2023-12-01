8e1eb273682f3dec1494c4fd9da5cfde6599edb2
gparap <gparap@protonmail.com>
2023-12-01 15:16:43 +0200

    Setup the basic game configuration & Display the background

--- three-heart-land-run/core/src/gparap/games/threeheartlandrun/ThreeHeartLandRun.java
+++ three-heart-land-run/core/src/gparap/games/threeheartlandrun/ThreeHeartLandRun.java
@@ -1,31 +1,57 @@
 package gparap.games.threeheartlandrun;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class ThreeHeartLandRun extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class ThreeHeartLandRun extends Game {
+	private SpriteBatch spriteBatch;
+	private Texture img;
+	private OrthographicCamera camera;
+
+	//game dimensions
+	public static final int MIN_WIDTH = 640;
+	public static final int MIN_HEIGHT = 360;
+	public static final int MAX_WIDTH = 1920;
+	public static final int MAX_HEIGHT = 1080;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("kgs.png");
+		spriteBatch = new SpriteBatch();
+		img = new Texture("background-empty.png");
+
+		//setup the game camera
+		camera = new OrthographicCamera(MIN_WIDTH, MIN_HEIGHT);
+		camera.setToOrtho(false);
+		camera.position.set(MIN_WIDTH/2F, MIN_HEIGHT/2F,0F);
+		camera.update();
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		spriteBatch.setProjectionMatrix(camera.combined);
+		spriteBatch.begin();
+		spriteBatch.draw(img, 0, 0, camera.viewportWidth, camera.viewportHeight);
+		spriteBatch.end();
 	}
-	
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+
+		//update the game camera
+		camera.viewportWidth = width;
+		camera.viewportHeight = height;
+		camera.position.set(width/2F, height/2F,0F);
+		camera.update();
+	}
+
 	@Override
 	public void dispose () {
-		batch.dispose();
+		spriteBatch.dispose();
 		img.dispose();
 	}
 }
--- three-heart-land-run/desktop/src/gparap/games/threeheartlandrun/DesktopLauncher.java
+++ three-heart-land-run/desktop/src/gparap/games/threeheartlandrun/DesktopLauncher.java
@@ -1,15 +1,20 @@
 package gparap.games.threeheartlandrun;
 
+import static gparap.games.threeheartlandrun.ThreeHeartLandRun.MAX_HEIGHT;
+import static gparap.games.threeheartlandrun.ThreeHeartLandRun.MAX_WIDTH;
+import static gparap.games.threeheartlandrun.ThreeHeartLandRun.MIN_HEIGHT;
+import static gparap.games.threeheartlandrun.ThreeHeartLandRun.MIN_WIDTH;
+
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import gparap.games.threeheartlandrun.ThreeHeartLandRun;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("Three-Heart Land Run");
-		new Lwjgl3Application(new ThreeHeartLandRun(), config);
-	}
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(60);
+        config.setTitle("Three-Heart Land Run");
+        config.setWindowedMode(MIN_WIDTH, MIN_HEIGHT);
+        config.setWindowSizeLimits(MIN_WIDTH, MIN_HEIGHT, MAX_WIDTH, MAX_HEIGHT);
+        new Lwjgl3Application(new ThreeHeartLandRun(), config);
+    }
 }
